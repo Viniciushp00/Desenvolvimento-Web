9f5ef7894f3e44492d5dad36e8bb37e4348be2ea
Marcos Vinicius <viniciushp00@gmail.com>
2023-06-03 00:45:31 -0300

    Ajuste na tela e logica de criação do pedido

--- php/produto.php
+++ php/produto.php
@@ -2,22 +2,31 @@
 
 include("conexao.php");
 
-if(isset($_POST['area']) || isset($_POST['cpf']) || isset($_POST['descrição-pedido']) || isset($_POST['data-solicitada-entrega'])){
-    
-        /*Limpando e-mail e senha para ninguem conseguir acessar sem um email ou senha valida*/ 
-        $area = $mysqli->real_escape_string($_POST['area']);
-        $cpf = $mysqli->real_escape_string($_POST['cpf']);
-        $descrição_pedido = $mysqli->real_escape_string($_POST['descrição_pedido']);
-        $data_solicitada_entrega = $mysqli->real_escape_string($_POST['data_solicitada_entrega']);
-        $area = $mysqli->real_escape_string($_POST['area']);
+try{
+     
+    $area = $_POST['area'];
+    $cpf_ponto = $_POST['cpf'];
+    $descrição_pedido = $_POST['descrição_pedido'];
+    $preco = $_POST['preco'];
+    $data_solicitada_entrega = $_POST['data_solicitada_entrega'];
+    $area = $_POST['area'];
+    $titulo = $_POST['titulo'];
 
-        $sql_code = "INSERT INTO pedido (descricao_pe,data_entrega_pe,id_cliente,id_area,id_status) VALUES ('$descrição_pedido','$data_solicitada_entrega','$cpf','$area',1)";
-        $sql_query = $mysqli->query($sql_code) or die ("Falha na execução do código SQL ");
+    function limpar_texto($str){ 
+        return preg_replace("/[^0-9]/", "", $str); 
+    }
+
+    $cpf = limpar_texto($cpf_ponto);
+
+    $sql_code = "INSERT INTO site.pedido (titulo,descricao_pe,data_entrega_pe,preco,id_cliente,id_area,id_status) VALUES ('$titulo','$descrição_pedido','$data_solicitada_entrega','$preco','$cpf','$area',1)";
+    $sql_query = $conectar->query($sql_code) or die ("Falha na execução do código SQL ");
     
 
-    header('../html/produtos/produtos.php');
+    header('Location: ../html/hpc_cliente.php');
     
 }
-
+catch (PDOException $e) {
+    echo ("Erro: ". $e->getMessage());
+}
 
 ?>
