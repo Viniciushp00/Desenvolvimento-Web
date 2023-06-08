39869dfd08b9ce5e9e85b22b394b6ca58ccca8bf
Marcos Vinicius <viniciushp00@gmail.com>
2023-06-08 17:41:43 -0300

    Deixando a index  responsivo

--- js/hpc_cliente.js
+++ js/hpc_cliente.js
@@ -1,16 +1,28 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
+  slidesPerView: getSlidesPerView(),
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+function getSlidesPerView() {
+  if (window.innerWidth < 700) {
+    return 1;
+  } else {
+    return 3;
+  }
+}
+
+window.addEventListener("resize", function() {
+  swiper.params.slidesPerView = getSlidesPerView();
+  swiper.update();
+});
